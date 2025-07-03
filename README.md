3d4c409a3a9e30bb60b963cc4989a9d2e5b96847
Felipe Rodrigo de Souza Gonçalves <f_g@outlook.com.br>
2025-07-03 14:11:46 -0300

    feat: cria entity e ajust arquivos para usar banco de dados no postgres (#3)
    * refactor: muda nomes de Tarefas -> Tasks (#1)
    
    * docs: comentários em algumas linhas
    
    * feat: cria entity e ajust arquivos para usar banco de dados no postgres
    
    * Merge branch 'main' into feat/cria-entity

--- README.md
+++ README.md
@@ -99,4 +99,3 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 ## License
 
 Nest is [MIT licensed](https://github.com/nestjs/nest/blob/master/LICENSE).
->>>>>>> dev
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module'; // é necessário importar todos os módulos no topo para depois importar no corpo
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({ // no código, é possível ter mais de um módulo, por isso importamos todos eles no app.module.ts
-  imports: [TasksModule],
+  imports: [TasksModule,
+      TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DBUSER,
+      password: process.env.DBPASSWORD,
+      database: 'todo_list',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false, // só use true em dev, cria tabelas automaticamente
+    })]
 })
 export class AppModule {}
